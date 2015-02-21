76a56c8ee27d62a41f9354ec54d7e045ff6c8457
David <david.emil.svensson@gmail.com>
2015-02-21 21:20:33 +0100

    ?

--- solveVC.m
+++ solveVC.m
@@ -14,13 +14,13 @@ beta2 = 0.3334;
 
 [n m] = size(psi_r);
 
-r_s = (3/(4*pi*2*abs(psi_r).^2))^(1/3);
+r_s = (3./(4*pi*2*abs(psi_r).^2)).^(1/3);
 index = find(r_s >= 1);
 e_c = zeros(1,m); 
 de_c = zeros(1,m);
 
-e_c(1:index-1) = A*log(r_s(1:index-1))+B+C*r_s(1:index-1)*log(rs(1:index-1)) +D*r_s(1:index-1);
-e_c(index:end) = gamma/(1 + beta1*sqrt(r_s(index:end)) + beta2*r_s(index:end));
+e_c(1:index-1) = A.*log(r_s(1:index-1))+B+C.*r_s(1:index-1).*log(r_s(1:index-1)) +D*r_s(1:index-1);
+e_c(index:end) = gamma./(1 + beta1*sqrt(r_s(index:end)) + beta2*r_s(index:end));
 
 de_c(1:index-1) = (A + C*r_s(1:index-1) + D*r_s(1:index-1))/r_s(1:index-1) + C*log(r_s(1:index-1));
 de_c(index:end) = -gamma.*(beta1.*(1./2*sqrt(r_s(index:end))) + beta2)/(1 + beta1*sqrt(r_s(index:end)) + beta2*r_s(index:end)).^2;
