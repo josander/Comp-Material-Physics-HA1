36c049bc3fda4654895e020949b53720ce1e7fd4
David <david.emil.svensson@gmail.com>
2015-02-22 14:20:47 +0100

    bug i solveVC

--- solveVC.m
+++ solveVC.m
@@ -15,10 +15,22 @@ beta2 = 0.3334;
 [n m] = size(psi_r);
 
 r_s = (3./(4*pi*2*abs(psi_r).^2)).^(1/3);
-index = find(r_s >= 1);
 e_c = zeros(1,m); 
 de_c = zeros(1,m);
 
+for i=1:m
+   if r_s(i) < 1
+       e_c(i) = A.*log(r_s(i))+B+C.*r_s(i).*log(r_s(i)) +D*r_s(i);
+       de_c(i) = (A + C*r_s(i) + D*r_s(i))/r_s(i) + C*log(r_s(i));
+   else
+       e_c(i) = gamma./(1 + beta1*sqrt(r_s(i)) + beta2*r_s(i));
+       de_c(i) = -gamma.*(beta1.*(1./2*sqrt(r_s(i))) + beta2)/(1 + beta1*sqrt(r_s(i)) + beta2*r_s(i)).^2;
+   end
+
+       
+       
+    
+end
 e_c(1:index-1) = A.*log(r_s(1:index-1))+B+C.*r_s(1:index-1).*log(r_s(1:index-1)) +D*r_s(1:index-1);
 e_c(index:end) = gamma./(1 + beta1*sqrt(r_s(index:end)) + beta2*r_s(index:end));
 
