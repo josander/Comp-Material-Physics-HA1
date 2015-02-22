ab183ef8f5c16f5e874efabb3b56d936f8b69026
Josephine <jossan_cuellar@hotmail.com>
2015-02-22 14:08:48 +0100

    Correct Vx

--- solveVEx.m
+++ solveVEx.m
@@ -2,11 +2,9 @@ function [ VEx ] = solveVEx( psi_r )
 %SOLVEEX Summary of this function goes here
 %   psi_r : Normalised wavefunction for heilum
 %   VEx = ex +n *ex'
-%   ex = 3/4 * (3n/pi)^(1/3)
+%   ex = - 3/4 * (3n/pi)^(1/3)
 
-const = (-(3/pi)^(-2/3) - 3*(3/pi)^(1/3));
-
-VEx = (2.*abs(psi_r).^2).^(1/3)/4;
+VEx = - (6*abs(psi_r).^2/pi).^(1/3);
 
 end
 
