9ea6bd8f4b16ae4b1daf09e30602ad6775140d74
David <david.emil.svensson@gmail.com>
2015-02-23 20:17:05 +0100

    task 5 är eeeegddd

--- getEp.m
+++ getEp.m
@@ -34,8 +34,7 @@ if c == 1
 end
 
 % Calculate epsilon
-%Ep(2:m-1) = -(3/4)*(3*abs(psi_r(2:end-1)).^2/pi).^(1/3) + e_c;
-Ep(2:m-1) = -(3/4)*(3*abs(psi_r(2:end-1)).^2/pi).^(1/3) + e_c;
+Ep(2:m-1) = -(3/4)*(3*2*abs(psi_r(2:end-1)).^2/pi).^(1/3) + e_c;
 
 Ep(m) = 0;
 
